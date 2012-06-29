68c2f918fb212aacad54d1f3152f554d7f0bdb6a
croustibat <bb@deezer.com>
2012-06-29 15:29:11 +0200

    add html part in example script

--- php/show-tracks-sample.php
+++ php/show-tracks-sample.php
@@ -15,8 +15,7 @@ for($i=0; $i<5; $i++){
         
 	$track =& $listTracks->data[$i];
         
-        $file = getSample($track->id); // here is the OGG part, make sure you chmod your dir before
-        $file_url = "/dzogg/".$file;    
+        $file_url = getSample($track->id); // here is the OGG part, make sure you chmod your dir before
         
         $oggTracks[] = array(
           "title"         => $track->title,
@@ -27,3 +26,30 @@ for($i=0; $i<5; $i++){
           "oggurl"        => $file_url
         );
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>OGG Deezer Preview</title>
+  </head>
+
+  <body>
+
+    <?php foreach($oggTracks as $ogg) { ?>
+      <div class="well">
+        <strong><?php echo $ogg['artiste_name'];?> - <?php echo $ogg['title'];?></strong>
+        <em><?php echo $ogg['album_name'];?></em>
+        <br />
+        <img src="<?php echo $ogg['cover'];?>" width="96" />
+        <br />
+        <audio controls="controls">
+          <source src="<?php echo $ogg['oggurl']?>" type="audio/ogg" />
+          Your browser does not support the audio tag.
+        </audio>
+      </div>
+    <?php } ?>
+
+  </body>
+</html>
